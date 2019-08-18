6f38e1c938a74dc11a2ba2817e3607a4241d270b
Joseph Kiehna <jhkiehna@gmail.com>
2019-08-18 19:02:11 -0400

    added soundclip routes, re organized routes. fixed user response object to return the api token

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,22 @@ Route::prefix('auth')->group(function () {
     Route::middleware('auth')->post('sign-out', 'SignInController@destroy');
 });
 
-Route::prefix('boards')->middleware('auth')->group(function () {
-    Route::get('/', 'BoardController@index');
-    Route::post('/', 'BoardController@store');
-    Route::get('/{board}', 'BoardController@show');
-    Route::patch('/{board}', 'BoardController@update');
-    Route::delete('/{board}', 'BoardController@destroy');
+Route::middleware('auth')->group(function () {
+    Route::prefix('boards')->group(function () {
+        Route::get('/', 'BoardController@index');
+        Route::post('/', 'BoardController@store');
+        Route::get('/{board}', 'BoardController@show');
+        Route::patch('/{board}', 'BoardController@update');
+        Route::delete('/{board}', 'BoardController@destroy');
+    });
+
+    Route::prefix('soundclips')->group(function () {
+        Route::get('/', 'SoundCliipController@index');
+        Route::post('/', 'SoundCliipController@store');
+        Route::get('/{soundClip}', 'SoundCliipController@show');
+        Route::patch('/{soundClip}', 'SoundCliipController@update');
+        Route::delete('/{soundClip}', 'SoundCliipController@destroy');
+    });
 });
+
+
